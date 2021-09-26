b1d41d5f487eec851beb2fac210e03c7c5b17f52
jmelendezartist <91428532+jmelendezartist@users.noreply.github.com>
2021-09-26 13:01:37 -0400

    Add files via upload

---
+++ cameraFollow.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class cameraFollow : MonoBehaviour {
+
+
+	public Transform myTarget;
+
+
+
+
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+
+	void Update () {
+
+
+		if(myTarget != null){
+			
+			Vector3 targPos = myTarget.position;
+
+			targPos.z = transform.position.z;
+
+			transform.position = targPos;
+	}
+}
+}
